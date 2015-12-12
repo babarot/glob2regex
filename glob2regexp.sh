89422cc9e6f31103674ff4e2366848234eb57f2c
b4b4r07 <b4b4r07@gmail.com>
2015-12-13 00:39:01 +0900

    Add sh file

---
+++ glob2regexp.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+glob2regexp() {
+    local glob char
+
+    glob="$1"
+    if [[ -z $glob ]]; then
+        return 1
+    fi
+
+    printf "^"
+    for (( i=0; i<${#glob}; i++ )); do
+        char="${glob:$i:1}"
+        case $char in
+            \*)
+                printf '.*'
+                ;;
+            .)
+                printf '\.'
+                ;;
+            "{")
+                printf '('
+                ;;
+            "}")
+                printf ')'
+                ;;
+            ,)
+                printf '|'
+                ;;
+            \\)
+                printf '\\\\'
+                ;;
+            *)
+                printf "$char"
+                ;;
+        esac
+    done
+    printf "$\n"
+}
+
+glob2regexp "$@"
